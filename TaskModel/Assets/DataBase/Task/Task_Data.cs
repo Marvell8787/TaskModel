a41b27175d576485d450a523bff8c6288c25ec5f
Marvell8787 <wendy850122@gmail.com>
2019-05-23 15:50:39 +0800

    20190523
    Wait Learn Model Compelet
    Task Model 80%

--- TaskModel/Assets/DataBase/Task/Task_Data.cs
+++ TaskModel/Assets/DataBase/Task/Task_Data.cs
@@ -7,7 +7,7 @@ static class Task_Data{
     public static string[] Learn_Threshold = new string[7] { "None", "Level 1's Over", "Level 2's Over", "Level 3's Over", "Level 4's Over", "Level 5's Over", "Level 1~6're Over" };
     public static string[] Learn_Request = new string[7] { "Complete the Level 1！", "Complete the Level 2！", "Complete the Level 3！", "Complete the Level 4！", "Complete the Level 5！", "Complete the Level 6！", "Complete the Overall！" };
     public static string[] Learn_Reward = new string[7] { "Crystal", "Crystal", "Crystal", "Crystal", "Crystal", "Crystal", "Crystal" };
-    public static string[] Learn_Punishment = new string[7] { "Crystal", "Crystal", "Crystal", "Crystal", "Crystal", "crystal", "Crystal" };
+    public static string[] Learn_Punishment = new string[7] { "Crystal", "Crystal", "Crystal", "Crystal", "Crystal", "Crystal", "Crystal" };
 
     public static string[] Battle_Title = new string[3] { "Battle 1", "Battle 2", "Battle 3" };
     public static string[] Battle_Threshold = new string[3] { "None", "Battle 1's Over", "Battle 2's Over"};
@@ -45,5 +45,16 @@ static class Task_Data{
     public static Task_Class Battle_Get(int n)
     {
         return battle_temp[n];
+    }
+    public static void ChangeStatus(string s, int c,int n)
+    {
+        if (s == "learn")
+        {
+            learn_temp[c].ChangeStatus(n);
+        }
+        else if (s == "battle")
+        {
+            battle_temp[c].ChangeStatus(n);
+        }
     }
 }
--- TaskModel/Assets/Script/Task/Function_Task.cs
+++ TaskModel/Assets/Script/Task/Function_Task.cs
@@ -5,7 +5,9 @@ using UnityEngine.UI;
 static class Check
 {
     public static string s = "";
+    public static int Choose = 0;
 }
+
 public class Function_Task : MonoBehaviour {
     void Start()
     {
@@ -13,9 +15,19 @@ public class Function_Task : MonoBehaviour {
 
         Task_Data.Learn_Init();
         Task_Data.Battle_Init();
+
+        Button b_temp;
+        b_temp = GameObject.Find("Button_Accept").GetComponent<Button>();
+        b_temp.interactable = false;
+        b_temp = GameObject.Find("Button_Finish").GetComponent<Button>();
+        b_temp.interactable = false;
     }
-    public void GetLearnData()
+    public void GetLearnData() //獲取 LearnData
     {
+        Button b_temp;
+        b_temp = GameObject.Find("Button_Accept").GetComponent<Button>();
+        b_temp.interactable = false;
+
         Check.s = "learn";
         ClearAllText();
         Task_Class[] learn_temp = new Task_Class[7];
@@ -50,8 +62,12 @@ public class Function_Task : MonoBehaviour {
         }
     }
 
-    public void GetBattleData()
+    public void GetBattleData() //獲取 BattleData
     {
+        Button b_temp;
+        b_temp = GameObject.Find("Button_Accept").GetComponent<Button>();
+        b_temp.interactable = false;
+
         Check.s = "battle";
         ClearAllText();
         Task_Class[] battle_temp = new Task_Class[3];
@@ -87,11 +103,11 @@ public class Function_Task : MonoBehaviour {
         }
     }
 
-    public void BackHome()
+    public void BackHome() //回首頁
     {
         Debug.Log("BackHome");
     }
-    public void ClearAllText()
+    public void ClearAllText() //清除所有Text
     {
         Text t_temp;
         for(int i = 0; i < 7; i++)
@@ -108,33 +124,101 @@ public class Function_Task : MonoBehaviour {
         t_temp = GameObject.Find("Text_PunishmentContent").GetComponent<Text>();
         t_temp.text = "";
     }
+    //任務內容接下
     public void PressContent_1()
     {
-        ShowContent(Check.s, 0);
+        if (Check.s != "")
+        {
+            Check.Choose = 0;
+            Button_Change();
+            ShowContent(Check.s, 0);
+        }
     }
     public void PressContent_2()
     {
-        ShowContent(Check.s, 1);
+        if (Check.s != "")
+        {
+            Check.Choose = 1;
+            Button_Change();
+            ShowContent(Check.s, 1);
+        }
     }
     public void PressContent_3()
     {
-        ShowContent(Check.s, 2);
+        if (Check.s != "")
+        {
+            Check.Choose = 2;
+            Button_Change();
+            ShowContent(Check.s, 2);
+        }
     }
     public void PressContent_4()
     {
-        ShowContent(Check.s, 3);
+        if (Check.s != "")
+        {
+            Check.Choose = 3;
+            Button_Change();
+            ShowContent(Check.s, 3);
+        }
     }
     public void PressContent_5()
     {
-        ShowContent(Check.s, 4);
+        if (Check.s != "")
+        {
+            Check.Choose = 4;
+            Button_Change();
+            ShowContent(Check.s, 4);
+        }
     }
     public void PressContent_6()
     {
-        ShowContent(Check.s, 5);
+        if (Check.s != "")
+        {
+            Check.Choose = 5;
+            Button_Change();
+            ShowContent(Check.s, 5);
+        }
     }
     public void PressContent_7()
     {
-        ShowContent(Check.s, 6);
+        if (Check.s != "")
+        {
+            Check.Choose = 6;
+            Button_Change();
+            ShowContent(Check.s, 6);
+        }
+    }
+
+    public void Button_Change()
+    {
+        int n;
+        Task_Class task_temp = new Task_Class();
+        if (Check.s == "learn")
+            task_temp = Task_Data.Learn_Get(Check.Choose);
+        else if (Check.s == "battle")
+            task_temp = Task_Data.Battle_Get(Check.Choose);
+
+        Button b_temp;
+        n = task_temp.GetStatus();
+
+        if (n > 1 && n < 4)
+        {
+            b_temp = GameObject.Find("Button_Accept").GetComponent<Button>();
+            b_temp.interactable = false;
+
+            b_temp = GameObject.Find("Button_Finish").GetComponent<Button>();
+            b_temp.interactable = true;
+        }
+        else if (n == 0)
+        {
+            b_temp = GameObject.Find("Button_Accept").GetComponent<Button>();
+            b_temp.interactable = true;
+        }
+        else if (n == 1)
+        {
+            b_temp = GameObject.Find("Button_Accept").GetComponent<Button>();
+            b_temp.interactable = false;
+        }
     }
 
     public void ShowContent(string s,int n)
@@ -154,5 +238,46 @@ public class Function_Task : MonoBehaviour {
         t_temp.text = task_temp.GetReward();
         t_temp = GameObject.Find("Text_PunishmentContent").GetComponent<Text>();
         t_temp.text = task_temp.GetPunishment();
+    }
+    public void Accept()
+    {
+        Button b_temp;
+        Text t_temp;
+        Task_Data.ChangeStatus(Check.s,Check.Choose,1);
+        Task_Class task_temp = new Task_Class();
+
+        if (Check.s == "learn")
+            task_temp = Task_Data.Learn_Get(Check.Choose);
+        else if (Check.s == "battle")
+            task_temp = Task_Data.Battle_Get(Check.Choose);
+
+        b_temp = GameObject.Find("Button_Accept").GetComponent<Button>();
+        b_temp.interactable = false;
+
+        t_temp = GameObject.Find("Text_Task_" + (Check.Choose + 1).ToString()).GetComponent<Text>();
+        t_temp.text = task_temp.GetTitle();
+        t_temp.color = Color.blue;
+
+    }
+    public void Finish()
+    {
+        Button b_temp;
+        Text t_temp;
+        Task_Data.ChangeStatus(Check.s, Check.Choose, 4);
+        Task_Class task_temp = new Task_Class();
+
+        if (Check.s == "learn")
+            task_temp = Task_Data.Learn_Get(Check.Choose);
+        else if (Check.s == "battle")
+            task_temp = Task_Data.Battle_Get(Check.Choose);
+
+        b_temp = GameObject.Find("Button_Finish").GetComponent<Button>();
+        b_temp.interactable = false;
+
+        t_temp = GameObject.Find("Text_Task_" + (Check.Choose + 1).ToString()).GetComponent<Text>();
+        t_temp.text = task_temp.GetTitle();
+        t_temp.color = Color.gray;
+        t_temp.fontStyle = FontStyle.Italic;
+        
     }
 }
